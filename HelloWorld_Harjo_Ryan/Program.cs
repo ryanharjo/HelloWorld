6c73c717332c3d18b67fc1ff2b0a15a460f9b997
Ryan Harjo <183425541+ryanharjo@users.noreply.github.com>
2025-02-18 11:04:57 -0600

    Add files via upload

---
+++ HelloWorld_Harjo_Ryan/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld_Harjo_Ryan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
